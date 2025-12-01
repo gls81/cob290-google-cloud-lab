e3b6b1db44a885b949bdaecef344e41406436481
cogs3 <g.storey@lboro.ac.uk>
2025-12-01 12:06:05 +0000

    Added initial files

---
+++ db-test.php
@@ -0,0 +1,16 @@
+<?php
+echo "COB290 Lab Test Database Connection";
+
+$servername = "localhost";
+$username = "lab";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
